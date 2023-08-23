24a1016905f67902f9921a7948f8a2360db41d00
JuanBonilla2006 <juancamilo23062006@gmail.com>
2023-08-23 10:44:32 -0500

    proyecto springboot

---
+++ src/main/java/com/example/turismoApp/modelos/Oferta.java
@@ -0,0 +1,18 @@
+package com.example.turismoApp.modelos;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "oferta")
+public class Oferta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String titulo;
+    private String descripcion;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private Double costoPersona;
+}
